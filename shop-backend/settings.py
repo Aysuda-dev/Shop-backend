8a90e962411501f243c969a8a7bf5862d5683781
Aysuda <zahrabadrloo2003@gmail.com>
2026-06-28 22:19:32 -0700

    Remove email password

--- shop-backend/settings.py
+++ shop-backend/settings.py
@@ -160,7 +160,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'shop.project.demo@gmail.com'
-EMAIL_HOST_PASSWORD = 'vrljbubnetvzlaky'
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 
 
