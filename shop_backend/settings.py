23e778c8dec9a2eeb9e67e50a4ce6f77e6f27c95
Aysuda <zahrabadrloo2003@gmail.com>
2026-07-02 02:23:00 -0700

    Configure static files

--- shop_backend/settings.py
+++ shop_backend/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/uploads/'
 MEDIA_ROOT= BASE_DIR / 'uploads'
