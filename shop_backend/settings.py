054931109091ecaa367f561329de4a19c7261743
Aysuda <zahrabadrloo2003@gmail.com>
2026-07-05 06:14:40 -0700

    Email debug

--- shop_backend/settings.py
+++ shop_backend/settings.py
@@ -170,7 +170,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'shop.project.demo@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
-
+EMAIL_TIMEOUT =10
 
 
 import locale
--- utils/send_email.py
+++ utils/send_email.py
@@ -3,11 +3,15 @@ from django.template.loader import render_to_string
 from django.utils.html import strip_tags
 from django.conf import settings
 
-def send_email( subject,to,context ,template):
-    try:
-        html_message = render_to_string( template, context )
-        plain_message = strip_tags(html_message)
-        from_email = settings.EMAIL_HOST_USER
-        send_mail(subject, plain_message, from_email, [to], html_message=html_message)
-    except Exception as e:
-        print(e)
+def send_email(subject, to, context, template):
+    html_message = render_to_string(template, context)
+    plain_message = strip_tags(html_message)
+
+    send_mail(
+        subject=subject,
+        message=plain_message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=[to],
+        html_message=html_message,
+        fail_silently=False,
+    )
