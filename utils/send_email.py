6faef8bc7030412317c6aee2a85bcd91f246abb0
Aysuda <zahrabadrloo2003@gmail.com>
2026-07-06 03:16:50 -0700

    update project dependenciies

--- utils/send_email.py
+++ utils/send_email.py
@@ -1,17 +1,18 @@
-from django.core.mail import send_mail
+import resend
+import os
 from django.template.loader import render_to_string
-from django.utils.html import strip_tags
-from django.conf import settings
+
+
+resend.api_key = os.environ.get("RESEND_API_KEY")
+
 
 def send_email(subject, to, context, template):
-    html_message = render_to_string(template, context)
-    plain_message = strip_tags(html_message)
 
-    send_mail(
-        subject=subject,
-        message=plain_message,
-        from_email=settings.EMAIL_HOST_USER,
-        recipient_list=[to],
-        html_message=html_message,
-        fail_silently=False,
-    )
+    html_message = render_to_string(template, context)
+    params = {
+        "from": "onboarding@resend.dev",  # بعداً دامین خودت را متصل کن
+        "to": [to],
+        "subject": subject,
+        "html": html_message,
+    }
+    resend.Emails.send(params)
